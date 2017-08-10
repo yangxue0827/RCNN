434ed10fbd9b0f96ac758c245a0483b192428c9e
yangxue0827 <yangxue0827@126.com>
2017-08-10 09:31:20 +0800

    config of path and files

---
+++ config.py
@@ -0,0 +1,10 @@
+# config path and files
+IMAGE_SIZE = 224
+SAVE_MODEL_PATH = './pre_train_model/model_save.model'
+FINE_TUNE_MODEL_PATH = './fine_tune_model/fine_tune_model_save.model'
+TRAIN_LIST = './train_list.txt'
+FINE_TUNE_LIST = './fine_tune_list.txt'
+FINE_TUNE_DATA = './data_set'
+TRAIN_SVM = './svm_train'
+TRAIN_CLASS = 17
+FINE_TUNE_CLASS = 3
